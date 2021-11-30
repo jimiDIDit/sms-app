8c98b0264f58dad546ed55f7c89c37621a34ace8
Jimi Flynn <18079822+jmbofly@users.noreply.github.com>
2021-11-30 10:41:36 -0500

    removed api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyADM7BtzEZTeTgytpDLbeUK3zzs5ldlyIY',
+    apiKey: '',
     authDomain: 'sure-marketing-solutions.firebaseapp.com',
     databaseURL: 'https://sure-marketing-solutions.firebaseio.com',
     projectId: 'sure-marketing-solutions',
